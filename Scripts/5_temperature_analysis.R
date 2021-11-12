db541a951cdbbfb70995cdefa329cdd7a4265c7a
Lisbeth Hordley <lhordley@butterfly-conservation.org>
2021-11-12 16:21:29 +0000

    New code to calculate direction and shift in temperature

--- Scripts/5_temperature_analysis.R
+++ Scripts/5_temperature_analysis.R
@@ -23,12 +23,12 @@ hec_records <- read.csv("Data/NMRS/Hectad_recording_levels_1975_1991_2012_2016.c
 
 ### first exclude migrant hectads from each nmrs dataset
 migrant_hectads_early[is.na(migrant_hectads_early)] <- 0
-nmrsdata_temp_early <- merge(nmrsdata_temp_early, migrant_hectads_early, by=c("Common_Name", "Hectad"), all=TRUE)
+nmrsdata_temp_early <- merge(nmrsdata_temp_early, migrant_hectads_early, by=c("Common_Name", "Hectad"), all.x=TRUE)
 nmrsdata_temp_early[is.na(nmrsdata_temp_early)] <- 0
 nmrsdata_temp_early <- nmrsdata_temp_early[nmrsdata_temp_early$Exclude !=1, ] ## removes 84 rows (now 165390)
 
 migrant_exclusion_late[is.na(migrant_exclusion_late)] <- 0
-nmrsdata_temp_late <- merge(nmrsdata_temp_late, migrant_exclusion_late, by=c("Common_Name", "Hectad"), all=TRUE)
+nmrsdata_temp_late <- merge(nmrsdata_temp_late, migrant_exclusion_late, by=c("Common_Name", "Hectad"), all.x=TRUE)
 nmrsdata_temp_late[is.na(nmrsdata_temp_late)] <- 0
 nmrsdata_temp_late <- nmrsdata_temp_late[nmrsdata_temp_late$Exclude !=1, ] ## removes 45 rows (now 390587)
 
@@ -95,7 +95,7 @@ rec_hecs <- ggplot() +
                fill = 'gray90', color = 'black') + 
   coord_fixed(ratio = 1.3, xlim = c(-10,3), ylim = c(50, 59)) + 
   theme_void() + 
-  geom_point(data =nmrsdata_temp_rec, 
+  geom_point(data =nmrsdata_temp_final, 
              aes(x = lon, y=lat), size=1) +
   theme(title = element_text(size = 12))
 rec_hecs
@@ -119,6 +119,76 @@ ggplot(data = nmrsdata_temp_rec[nmrsdata_temp_rec$groups==2,], mapping = aes(x =
   theme_light()
 #
 
+#### Direction and distance of shift based on mean annual temperature
+## 25th and 75th percentiles for each species in TP1
+detach(package:plyr)
+tp1_species_percentiles <- nmrsdata_temp_rec %>% group_by(Common_Name) %>% filter(time_period=="TP1") %>%
+  summarise(temp_25_perc = quantile(temperature, probs = 0.025),
+            temp_75_perc = quantile(temperature, probs = 0.975))
+nmrsdata_temp_rec <- merge(nmrsdata_temp_rec, tp1_species_percentiles, by="Common_Name")
+
+## find hectads in TP2 which fall within 25th and 75th percentiles for each species
+filtered <- nmrsdata_temp_rec %>% group_by(Common_Name, time_period) %>%
+  filter(temperature >= temp_25_perc & temperature <= temp_75_perc)
+
+## calculate centroids of each
+temp_centroid_rec <- filtered %>% group_by(Common_Name, time_period) %>%
+  dplyr::summarise(mean_lat=mean(lat), mean_lon=mean(lon), mean_easting=mean(easting), mean_northing=mean(northing))
+
+## change orientation of data
+temp_centroid_rec2 <- temp_centroid_rec %>%
+  gather(key, value, -Common_Name, -time_period) %>%
+  unite(col, key, time_period) %>%
+  spread(col, value)
+## NAs exist either because:
+# Species are only found in one hectad in TP1 => 25th & 75th percentile the same value, and no exact match in TP2
+# No hectads fall between 25th and 75th percentiles in TP2
+
+## remove NAs
+temp_centroid_rec2 <- na.omit(temp_centroid_rec2) ## 63 species (7)
+
+# then calculate distance (magnitude) and direction of shift between time periods for each species
+# Use pythagoras equation
+temp_centroid_rec2$distance <- sqrt((temp_centroid_rec2$mean_northing_TP1-temp_centroid_rec2$mean_northing_TP2)^2 + 
+                                      (temp_centroid_rec2$mean_easting_TP1-temp_centroid_rec2$mean_easting_TP2)^2) ## pythagoras equation
+temp_centroid_rec2$distance <- temp_centroid_rec2$distance/1000
+## this gives distance in km
+
+# use mean lat and lon values to calculate bearing/direction using geosphere package
+library(geosphere)
+temp_centroid_rec2$bearing <- bearing(temp_centroid_rec2[,c(6,4)], temp_centroid_rec2[,c(7,5)])
+temp_centroid_rec2$direction <- (temp_centroid_rec2$bearing + 360) %% 360 # add full circle, i.e. +360, and determine modulo for 360
+
+## plot result
+rec_centroid <- ggplot(data=temp_centroid_rec2,
+                       aes(x=direction, y=distance)) +
+  geom_segment(aes(xend = direction, yend = 0.1)) +
+  geom_point() +
+  scale_x_continuous(limits = c(0,360),
+                     breaks = seq(0, 360, by = 45),
+                     minor_breaks = seq(0, 360, by = 15)) +
+  # scale_y_log10() +
+  coord_polar() +
+  theme_bw()
+rec_centroid
+ggsave(rec_centroid, file="Graphs/multidirectional_temp_rec.png")
+
+## look at chestnut coloured carpet
+worldmap = map_data('world')
+ggplot() + 
+  geom_polygon(data = worldmap, 
+               aes(x = long, y = lat, group = group), 
+               fill = 'gray90', color = 'black') + 
+  coord_fixed(ratio = 1.3, xlim = c(-10,3), ylim = c(50, 59)) + 
+  geom_point(data = nmrsdata_temp_rec[nmrsdata_temp_rec$Common_Name=="Angle-striped Sallow",], 
+             aes(x = as.numeric(lon), 
+                 y = as.numeric(lat), colour=time_period), size=1) + 
+  theme_void() +
+  theme(title = element_text(size = 12))
+
+### this method is very biased to recording effort - if the hectads with the temperature values
+## between percentiles are not recorded, it won't give a true indication of what direction a 
+## species should shift under climate change
 
 ## Well and heavily recorded hectads
 well_heavy_hecs <- hec_records %>%
@@ -181,6 +251,61 @@ ggplot(data = nmrsdata_temp_wh[nmrsdata_temp_wh$groups==2,], mapping = aes(x = r
   #scale_y_continuous(breaks = seq(3, 11, by = 1)) +
   theme_light()
 #
+
+#### Direction and distance of shift based on mean annual temperature
+## 25th and 75th percentiles for each species in TP1
+detach(package:plyr)
+tp1_species_percentiles <- nmrsdata_temp_wh %>% group_by(Common_Name) %>% filter(time_period=="TP1") %>%
+  summarise(temp_25_perc = quantile(temperature, probs = 0.25),
+            temp_75_perc = quantile(temperature, probs = 0.75))
+nmrsdata_temp_wh <- merge(nmrsdata_temp_wh, tp1_species_percentiles, by="Common_Name")
+
+## find hectads in TP2 which fall within 25th and 75th percentiles for each species
+filtered <- nmrsdata_temp_wh %>% group_by(Common_Name, time_period) %>%
+  filter(temperature >= temp_25_perc & temperature <= temp_75_perc)
+
+## calculate centroids of each
+temp_centroid_well <- filtered %>% group_by(Common_Name, time_period) %>%
+  dplyr::summarise(mean_lat=mean(lat), mean_lon=mean(lon), mean_easting=mean(easting), mean_northing=mean(northing))
+
+## change orientation of data
+temp_centroid_well2 <- temp_centroid_well %>%
+  gather(key, value, -Common_Name, -time_period) %>%
+  unite(col, key, time_period) %>%
+  spread(col, value)
+## NAs exist either because:
+# Species are only found in one hectad in TP1 => 25th & 75th percentile the same value, and no exact match in TP2
+# No hectads fall between 25th and 75th percentiles in TP2
+
+## remove NAs
+temp_centroid_well2 <- na.omit(temp_centroid_well2) ## 50 species (lose 16 species)
+
+# then calculate distance (magnitude) and direction of shift between time periods for each species
+# Use pythagoras equation
+temp_centroid_well2$distance <- sqrt((temp_centroid_well2$mean_northing_TP1-temp_centroid_well2$mean_northing_TP2)^2 + 
+                                       (temp_centroid_well2$mean_easting_TP1-temp_centroid_well2$mean_easting_TP2)^2) ## pythagoras equation
+temp_centroid_well2$distance <- temp_centroid_well2$distance/1000
+## this gives distance in km
+
+# use mean lat and lon values to calculate bearing/direction using geosphere package
+library(geosphere)
+temp_centroid_well2$bearing <- bearing(temp_centroid_well2[,c(6,4)], temp_centroid_well2[,c(7,5)])
+temp_centroid_well2$direction <- (temp_centroid_well2$bearing + 360) %% 360 # add full circle, i.e. +360, and determine modulo for 360
+
+## plot result
+well_centroid <- ggplot(data=temp_centroid_well2,
+                        aes(x=direction, y=distance)) +
+  geom_segment(aes(xend = direction, yend = 0.1)) +
+  geom_point() +
+  scale_x_continuous(limits = c(0,360),
+                     breaks = seq(0, 360, by = 45),
+                     minor_breaks = seq(0, 360, by = 15)) +
+  # scale_y_log10() +
+  coord_polar() +
+  theme_bw()
+well_centroid
+ggsave(well_centroid, file="Graphs/multidirectional_temp_well.png")
+
 
 
 
